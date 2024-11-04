0256f532591e2290998c9be889b0fbc2514c8800
curtiswiser <curtiswiser22@gmail.com>
2024-11-04 15:33:23 -0700

    added in forward and backward logic

--- player.py
+++ player.py
@@ -20,11 +20,20 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation +=  PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
         if keys[pygame.K_a]:
             self.rotate(dt = -dt)
         if keys[pygame.K_d]:
             self.rotate(dt = dt)
+        if keys[pygame.K_w]:
+            self.move(dt = dt)
+        if keys[pygame.K_s]:
+            self.move(dt = -dt)
+
+    
