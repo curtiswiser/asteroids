bd903d3e321ab2b6222a6f066916e5e05dcaa706
curtiswiser <curtiswiser22@gmail.com>
2024-11-04 08:13:16 -0700

    added in a game loop and created a .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import pygame
 from constants import *
 
 def main(): 
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen_color = (0,0,0)
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #infinate gaming loop
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(screen_color)
+        pygame.display.flip()
+        
+        
 
 if __name__== "__main__":
     main()
