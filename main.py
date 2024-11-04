3a6d1e21bd4c76c18119966594691ad5abb87fc8
curtiswiser <curtiswiser22@gmail.com>
2024-11-04 16:26:13 -0700

    created objects for updatables and drawables

--- main.py
+++ main.py
@@ -9,21 +9,30 @@ def main():
     dt = 0
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
 
+    #create groups: 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
+
     #infinate gaming loop
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                   
+        
         milliseconds = clock.tick(60)#this sets the frame rate to 60 fps
         dt = milliseconds/1000 #converts the value to seconds. used to align the functions to the frame rate
 
-        player.update(dt = dt)#update game object
+        for obj in updatable:
+            obj.update(dt = dt)
         screen.fill("black")#clear screen
-        player.draw(screen=screen) #draw everything
 
+        for obj in drawable:
+            obj.draw(screen = screen)
+      
         pygame.display.flip()
-
 
 if __name__== "__main__":
     main()
